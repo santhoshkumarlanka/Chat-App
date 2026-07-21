d1b6a61853e36ca81262ddeba105cae05e3f97fe
santhoshkumarlanka <santhoshkumarl2005@gmail.com>
2026-07-21 18:26:45 +0530

    "page allignment"

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,15 +10,11 @@ const HomePage = () => {
   const { selectedUser, selectedGroup } = useChatStore();
 
   return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
+    <div className="h-screen pt-16 bg-base-200">
+      <div className="flex h-full w-full bg-base-100 shadow-sm overflow-hidden">
+        <Sidebar />
 
-            {!selectedUser && !selectedGroup ? <NoChatSelected /> : <ChatContainer />}
-          </div>
-        </div>
+        {!selectedUser && !selectedGroup ? <NoChatSelected /> : <ChatContainer />}
       </div>
     </div>
   );
